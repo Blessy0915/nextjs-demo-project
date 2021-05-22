49af4dc8bcd6bf7ea3be699f1f64cabc0a478ffc
Blessy0915 <blessyferns2597@gmail.com>
2021-05-22 23:32:01 +0530

    updated Home page with meta data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+
+      <Head>
+          <title>Next Demo Project</title>
+          <meta name='keywords' content='web development' /> 
+      </Head>
+      
       <h1>Welcome to Next</h1>
     </div>
   )
