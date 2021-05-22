ea5b4736626ab205b3a4980acb2a091f180b0f62
Blessy0915 <blessyferns2597@gmail.com>
2021-05-22 23:37:20 +0530

    Meta data for About page

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,12 @@
+import Head from 'next/head'
+
 const About = () => {
     return (
         <div>
+            <Head>
+                <title>About</title>
+            </Head>
+
             <h1>About Page</h1>
         </div>
     )
