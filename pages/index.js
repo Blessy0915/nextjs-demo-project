11524458c02a86f4a803481afa71f93c409ace9b
Blessy0915 <blessyferns2597@gmail.com>
2021-05-23 11:32:54 +0530

    using getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({articles}) {
   return (
     <div>
 
@@ -9,7 +9,17 @@ export default function Home() {
           <meta name='keywords' content='web development' /> 
       </Head>
       
-      <h1>Welcome to Next</h1>
+      { articles.map(article => <h2>{article.title}</h2>)}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+    const articles = await res.json()
+    return {
+      props : {
+        articles
+      }
+    }
 }
