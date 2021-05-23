f40e2ff1a7830d90abe3c6819da4ec450c15e9e0
Blessy0915 <blessyferns2597@gmail.com>
2021-05-23 10:03:28 +0530

    Nav Component & css

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,17 @@
+import Nav from './Nav'
 import layoutStyles from '../styles/Layout.module.css'
 
 const Layout = ({children}) => {
     return (
-        <div className={layoutStyles.container}>
+        <>
+            <Nav/>
+            
+            <div className={layoutStyles.container}>
             <main styles={layoutStyles.main}>
                 {children}
             </main>
         </div>
+        </>
     )
 }
 
