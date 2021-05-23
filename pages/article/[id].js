e2e6eae84922093aba181ca600b0af53085b54e2
Blessy0915 <blessyferns2597@gmail.com>
2021-05-23 13:43:16 +0530

    using combination of getStaticProps & getStaticPaths

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -14,13 +14,30 @@ const Article = ({article}) => {
     )
 }
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
     const article = await res.json()
     return {
         props : {
             article
         }
+    }
+}
+
+export const getStaticPaths = async () => {
+    //get all posts
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const articles = await res.json()
+
+    //generate ids array
+    const ids = articles.map(article => article.id)
+
+    //generate paths
+    const paths = ids.map(id => ({params : {id : id.toString()}}))
+
+    return {
+        paths,
+        fallback : false
     }
 }
 export default Article
